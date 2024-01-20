6b4e9874b181f10e8255d1c1b28d42365eb3efb5
unknown <vuthuong410@gmail.com>
2024-01-20 16:51:22 +0700

    first commit

--- Buổi 6/6Bai1.js
+++ Buổi 6/6Bai1.js
@@ -1,33 +1,53 @@
-function validateForm(){
+function validateForm() {
     var x;
-    x = document.forms["dieukien"]["textho"].value;
-    if(x == " "){
-        alert("Bạn chưa nhập họ");
+    var y;
+    var z;
+    x = document.forms["Form"]["txtho"].value;
+    y = document.forms["Form"]["txtten"].value;
+    z = document.getElementById("txtdienthoai").value;
+    if (x == "") {
+    alert("Vui lòng nhập họ");
+    return false;
+    } else if (y == "") {
+    alert("Vui lòng nhập tên");
+    return false;
+    } else if (z == "") {
+        alert("Vui lòng nhập số điện thoại");
         return false;
-    }
-    x = document.getElementById("textten").value;
-    if(x == " "){
-        alert("Bạn chưa nhập tên");
+    } else if(isNaN(z) || z.length>10 || z.length<10){
+        alert("Số điện thoại không hợp lệ, mời nhập lại");
         return false;
+    } else {
+        alert("Nộp thành công");
     }
-    x = document.getElementById("dienthoai").value;
-    if(isNaN(x) || length(x) == 0 || length(x) > 10){
-        alert("Điện thoại phải là số, mời nhạp lại");
-        return false;
+ }
+const daySelect = document.querySelector("#ngay")
+for (let i = 1; i <= 31; i ++) {
+    const option = document.createElement("option")
+    option.value=i
+    option.innerHTML = i
+    if (i == 1) {
+        option.selected = true
     }
+    daySelect.appendChild(option)
 }
-function populateDropdown(select, start, end) {
-    for (let i = start; i <= end; i++) {
-      const option = document.createElement("option");
-      option.text = i;
-      option.value = i;
-      select.add(option);
+const monthSelect = document.querySelector("#thang")
+for (let i = 1; i <= 12; i ++) {
+    const option = document.createElement("option")
+    option.value=i
+    option.innerHTML = i
+    if (i == 1) {
+        option.selected = true
     }
+    monthSelect.appendChild(option)
 }
-const dobDaySelect = document.getElementById("dobDay");
-const dobMonthSelect = document.getElementById("dobMonth");
-const dobYearSelect = document.getElementById("dobYear");
-populateDropdown(dobDaySelect, 1, 31);
-populateDropdown(dobMonthSelect, 1, 12);
-populateDropdown(dobYearSelect, 1900, new Date().getFullYear());
-
+const yearSelect = document.querySelector("#nam")
+for (let i = 1980; i <= 2024; i ++) {
+    const option = document.createElement("option")
+    option.value=i
+    option.innerHTML = i
+    if (i == 1) {
+        option.selected = true
+    }
+    yearSelect.appendChild(option)
+}
